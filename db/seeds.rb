66ab5bd5a6fab1bcd174cfaf40584a70f24588fa
Obie Fernandez <obie@hashrocket.com>
2010-01-24 13:00:46 -0500

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 User.delete_all
-User.create(:login => 'admin',
+User.create!(:login => 'admin',
             :email => 'admin@tr3w.com',
-            :password => '123', :password_confirmation => '123'
+            :password => '1234', :password_confirmation => '1234',
             :authorized_approver => true)
 
 Client.delete_all      
-client = Client.create(:name => 'Taigan', :code => 'TAIGAN')
+client = Client.create!(:name => 'Taigan', :code => 'TAIGAN')
 
 BillingCode.delete_all
-client.billing_codes.create(:code => 'MTG', :description => 'Meetings')
-client.billing_codes.create(:code => 'DEV', :description => 'Development')
+client.billing_codes.create!(:code => 'MTG', :description => 'Meetings')
+client.billing_codes.create!(:code => 'DEV', :description => 'Development')
