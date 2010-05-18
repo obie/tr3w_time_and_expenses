436a1c346a337397f3da0a189d4fb03dbe4f8672
Bernerd Schaefer and Josh Graham <dev+bernerdschaefer+delitescere@hashrocket.com>
2010-05-18 16:21:59 -0500

    action_controller.session_store deprecated

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 module TimeAndExpenses
   class Application < Rails::Application
-    config.action_controller.session_store = :active_record_store
+    config.action_dispatch.session_store = :active_record_store
     config.generators do |g|
       g.test_framework :rspec
     end
