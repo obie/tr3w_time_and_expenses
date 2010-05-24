bd6a7b265031fb9b151f778d5a50f4060a184cbe
Bernerd Schaefer and Josh Graham <dev+bernerdschaefer+delitescere@hashrocket.com>
2010-05-24 14:42:48 -0500

    Add XML format to index

--- app/controllers/billing_codes_controller.rb
+++ app/controllers/billing_codes_controller.rb
@@ -1,3 +1,13 @@
 class BillingCodesController < BelongsToCrudController
   parent_model :Client
+
+  def index
+    @billing_codes = BillingCode.where(:client_id => params[:client_id])
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @billing_codes }
+    end
+  end
+
 end
