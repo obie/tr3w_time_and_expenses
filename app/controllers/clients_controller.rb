c4203339abab93b0fe64c505475b8dd9e2c76c48
Bernerd Schaefer and Josh Graham <dev+bernerdschaefer+delitescere@hashrocket.com>
2010-05-24 16:18:27 -0500

    Update controller to Rails 3 idioms

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,42 +1,27 @@
 class ClientsController < ApplicationController
+  respond_to :html, :xml
   # GET /clients
   # GET /clients.xml
   def index
     @clients = Client.where(params.except(:action, :controller, :format))
-
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml { render :xml => @clients.to_xml }
-    end
+    respond_with(@clients)
   end
 
   def recent
     @clients = Client.recent
-
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml  { render :xml => @clients.to_xml }
-    end
+    respond_with(@clients)
   end
 
   def newest
     @client = Client.recent.first
-
-    respond_to do |format|
-      format.html
-      format.xml { render :xml => @client }
-    end
+    respond_with(@client)
   end
-  
+
   # GET /clients/1
   # GET /clients/1.xml
   def show
     @client = Client.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.rhtml
-      format.xml  { render :xml => @client.to_xml }
-    end
+    respond_with(@client)
   end
 
   # GET /clients/new
@@ -53,34 +38,16 @@ class ClientsController < ApplicationController
   # POST /clients.xml
   def create
     @client = Client.new(params[:client])
-
-    respond_to do |format|
-      if @client.save
-        flash[:notice] = 'Client was successfully created.'
-        format.html { redirect_to client_path(@client) }
-        format.xml  { head :created, :location => @client }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @client.errors.to_xml, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Client was successfully created.' if @client.save
+    respond_with(@client)
   end
 
   # PUT /clients/1
   # PUT /clients/1.xml
   def update
     @client = Client.find(params[:id])
-
-    respond_to do |format|
-      if @client.update_attributes(params[:client])
-        flash[:notice] = 'Client was successfully updated.'
-        format.html { redirect_to client_path(@client) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @client.errors.to_xml }
-      end
-    end
+    flash[:notice] = 'Client was successfully updated.' if @client.update_attributes(params[:client])
+    respond_with(@client)
   end
 
   # DELETE /clients/1
@@ -88,10 +55,6 @@ class ClientsController < ApplicationController
   def destroy
     @client = Client.find(params[:id])
     @client.destroy
-
-    respond_to do |format|
-      format.html { redirect_to clients_path }
-      format.xml  { head :ok }
-    end
+    respond_with(@client)
   end
 end
