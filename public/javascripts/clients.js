47f010e3ac7e2f97b8679221fef3c192d5c36bd1
Bernerd Schaefer and Josh Graham <dev+bernerdschaefer+delitescere@hashrocket.com>
2010-06-04 16:20:18 -0500

    add comment

--- public/javascripts/clients.js
+++ public/javascripts/clients.js
@@ -1,5 +1,6 @@
 $(function() {
   $("#update_draft_timesheets").bind("ajax:success", function(event, data) {
+    // assumes JSON is the repsonse content type
     $(data).each(function() {
       var td = $('#client_' + this.id + ' .draft_timesheets_count')
       td.html(this.draft_timesheets_count);
